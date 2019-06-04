632d6e7d4e49e5323f897443e44d6526a19129da
Marin Šimić <marin.simic@fer.hr>
2019-06-04 21:07:31 +0200

    fetcher

--- app/src/main/java/com/example/mreznikviz/net/QuizzFetcher.kt
+++ app/src/main/java/com/example/mreznikviz/net/QuizzFetcher.kt
@@ -1,21 +1,21 @@
 package com.example.mreznikviz.net
 
 import android.os.AsyncTask
-import com.example.mreznikviz.entities.Quizz
+import com.example.mreznikviz.entities.JsonCategory
 
-class QuzzFetcher : AsyncTask<Long?, Void, Quizz?>() {
+class QuzzFetcher : AsyncTask<Long?, Void, JsonCategory?>() {
 
 
-    override fun doInBackground(vararg params: Long?): Quizz? {
+    override fun doInBackground(vararg params: Long?): JsonCategory? {
         var rest = RestFactory.instance
-        var quizzQuestionList: Quizz? = null
+        var quizzQuestionList: JsonCategory? = null
         for (param in params){
             quizzQuestionList = rest.getQuizzQuestions(param)
         }
         return quizzQuestionList
     }
 
-    override fun onPostExecute(result: Quizz?) {
-        
+    override fun onPostExecute(result: JsonCategory?) {
+        print(result?.title)
     }
 }
