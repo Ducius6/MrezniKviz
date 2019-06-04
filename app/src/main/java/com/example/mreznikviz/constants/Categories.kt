46fc775d3cb10233aa66e37f629a2ea648f741cf
Luka Martic <lukamarticc@gmail.com>
2019-06-04 21:05:59 +0200

    Categories

--- app/src/main/java/com/example/mreznikviz/constants/Categories.kt
+++ app/src/main/java/com/example/mreznikviz/constants/Categories.kt
@@ -1,7 +1,7 @@
 package com.example.mreznikviz.constants
 
-enum class Categories(val id: Int) {
-    POP_MUSIC(770),
-    SCIENCE(25),
-    FOUR_LETTER_WORDS(51)
+enum class Categories(val id: Int, val title: String) {
+    POP_MUSIC(770, "Pop music"),
+    SCIENCE(25, "Science"),
+    FOUR_LETTER_WORDS(51, "4 letter words")
 }
