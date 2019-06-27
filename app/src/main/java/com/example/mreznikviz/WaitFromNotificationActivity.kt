800fc69d6b8d4de62ab9a4b27bf0a5b1039c7f51
Luka Martic <lukamarticc@gmail.com>
2019-06-27 19:27:11 +0200

    This reverts commit e2e3b333ce012ba9b553ec4b55dcd83b24d221db.
    # Conflicts:
    #	app/src/main/java/com/example/mreznikviz/animations/BounceAnimation.kt

--- app/src/main/java/com/example/mreznikviz/WaitFromNotificationActivity.kt
+++ app/src/main/java/com/example/mreznikviz/WaitFromNotificationActivity.kt
@@ -1,10 +1,15 @@
 package com.example.mreznikviz
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mreznikviz.animations.BounceAnimation
 import com.example.mreznikviz.entities.Quizz
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_wait_from_notification.*
 
 class WaitFromNotificationActivity : AppCompatActivity() {
@@ -22,7 +27,14 @@ class WaitFromNotificationActivity : AppCompatActivity() {
         BounceAnimation(circleSmaller).withDuration(3300).isRepeatable(true).withAmplitude(0.5).executeSingleEvent()
         BounceAnimation(circleBigger).withDuration(3300).isRepeatable(true).withAmplitude(0.85).withDelay(450).executeSingleEvent()
 
-
-
+        val listener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    startActivity(Intent(this@WaitFromNotificationActivity, QuestionActivity::class.java))
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {}
+        }
+        FirebaseDatabase.getInstance().reference.child(quizz.id).child("start").addValueEventListener(listener)
     }
 }
