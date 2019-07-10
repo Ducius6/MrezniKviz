0f2aed418f58f78812237e80d47ec3945d05288f
Duje Drobac <dd50544@fer.hr>
2019-07-10 16:26:26 +0200

    Duje commit

--- app/src/main/java/com/example/mreznikviz/usernet/UserRestFactory.kt
+++ app/src/main/java/com/example/mreznikviz/usernet/UserRestFactory.kt
@@ -3,7 +3,6 @@ package com.example.mreznikviz.usernet
 import com.example.mreznikviz.usernet.userretrofit.UserRestRetrofit
 
 object UserRestFactory {
-    //192.168.0.14
     //10.0.2.2
     val BASE_IP = "10.0.2.2"
 
