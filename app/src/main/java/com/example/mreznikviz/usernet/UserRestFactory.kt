126d6621cd31c2127c33b7d7856a3ab49b9b97f8
Duje Drobac <dd50544@fer.hr>
2019-07-12 10:45:04 +0200

    Merge branch 'master' of https://gitlab.tel.fer.hr/ddrobac/mrezni-kviz
    # Conflicts:
    #	app/src/main/java/com/example/mreznikviz/usernet/UserRestFactory.kt

--- app/src/main/java/com/example/mreznikviz/usernet/UserRestFactory.kt
+++ app/src/main/java/com/example/mreznikviz/usernet/UserRestFactory.kt
@@ -4,7 +4,7 @@ import com.example.mreznikviz.usernet.userretrofit.UserRestRetrofit
 
 object UserRestFactory {
     //10.0.2.2
-    val BASE_IP = "192.168.0.14"
+    val BASE_IP = "10.0.2.2"
 
     val instance: UserRestInterface
         get() = UserRestRetrofit()
